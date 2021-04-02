c077087be73b4869c4e3695cd318bbeb0b1b54d0
Rock Binary <bptoreto9@gmail.com>
2021-04-01 21:42:12 -0500

    code collections

---
+++ src/basic_code/Collections.kt
@@ -0,0 +1,23 @@
+package basic_code
+
+fun main() {
+    val collecCity = Collections()
+    collecCity.collectionsCity()
+}
+
+
+
+class Collections {
+
+
+    fun collectionsCity(){
+        val cyties = listOf<String>("Santiago","Bogota","Brazilian","Tokyo","Tanzania")
+        println("the list cyties:: $cyties")
+        println()
+        println("------ filtro B ------")
+        cyties.filter { it.startsWith("B") }
+                .sortedBy { it }
+                .map { it.toUpperCase() }
+                .forEach{ println(it)}
+    }
+}
